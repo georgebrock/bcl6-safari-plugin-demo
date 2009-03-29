c54381e5fc2f6eb56ae3cd59894229294619a7b5
George Brocklehurst <george.brocklehurst@gmail.com>
2009-03-29 08:30:40 +0100

    Added a class and set the bundle's principle class and bundle identifier

---
+++ BCL6Demo.h
@@ -0,0 +1,16 @@
+//
+//  BCL6Demo.h
+//  BCL6Demo
+//
+//  Created by George on 29/03/2009.
+//  Copyright 2009 George Brocklehurst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BCL6Demo : NSObject {
+
+}
+
+@end
---
+++ BCL6Demo.m
@@ -0,0 +1,14 @@
+//
+//  BCL6Demo.m
+//  BCL6Demo
+//
+//  Created by George on 29/03/2009.
+//  Copyright 2009 George Brocklehurst. All rights reserved.
+//
+
+#import "BCL6Demo.h"
+
+
+@implementation BCL6Demo
+
+@end
